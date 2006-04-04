c7b79b0cce1a1abecd437b160a9d0abd24a50853
Tres Seaver <tseaver@palladion.com>
2006-04-04 17:39:32 +0000

    Release management for zope.deprecation-3.1.0 eggification.

--- setup.py
+++ setup.py
@@ -25,9 +25,9 @@ except ImportError, e:
     from distutils.core import setup, Extension
     
 setup(name='zope.deprecation',
-      version='3.0',
+      version='3.1.0',
 
-      url='http://svn.zope.org/zope.deprecation',
+      url='http://svn.zope.org/zope.deprecation/tags/3.1.0',
       license='ZPL 2.1',
       description='Zope 3 Deprecation Infrastructure',
       author='Zope Corporation and Contributors',
@@ -41,6 +41,7 @@ setup(name='zope.deprecation',
       packages=['zope', 'zope.deprecation'],
       namespace_packages=['zope',],
 
+      install_requires=['zope.proxy'],
       tests_require = ['zope.testing'],
       include_package_data = True,
       zip_safe = False,
