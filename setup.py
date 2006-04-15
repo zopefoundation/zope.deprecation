118acbaefda53b5cf191b035350032db91bb8487
Tres Seaver <tseaver@palladion.com>
2006-04-15 16:17:03 +0000

    'package_dir' must be a relative path.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ except ImportError, e:
     from distutils.core import setup, Extension
     
 setup(name='zope.deprecation',
-      version='3.0',
+      version='3.3-dev',
 
       url='http://svn.zope.org/zope.deprecation',
       license='ZPL 2.1',
@@ -37,7 +37,7 @@ setup(name='zope.deprecation',
                        "mark APIs and components which will be removed in "
                        "future releases.",
       
-      package_dir = {'': os.path.join(os.path.dirname(__file__), 'src')},
+      package_dir = {'': 'src'},
       packages=['zope', 'zope.deprecation'],
       namespace_packages=['zope',],
 
