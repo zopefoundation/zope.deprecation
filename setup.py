16718ef7daee43eff08ecaf488db36295deed7bf
Tres Seaver <tseaver@palladion.com>
2012-04-19 22:27:00 +0000

    Note PyPy support explicitly.

--- setup.py
+++ setup.py
@@ -50,6 +50,8 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
         ],
     package_dir = {'': 'src'},
     packages=find_packages('src'),
