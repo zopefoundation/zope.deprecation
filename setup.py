4d874be7dcde1b7518c8c497652d847696ed1eb4
Tres Seaver <tseaver@palladion.com>
2012-05-16 17:19:50 +0000

    svb

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def read(*rnames):
 
 setup(
     name='zope.deprecation',
-    version='4.0.0',
+    version='4.0.1dev',
     url='http://pypi.python.org/pypi/zope.deprecation',
     license='ZPL 2.1',
     description='Zope Deprecation Infrastructure',
